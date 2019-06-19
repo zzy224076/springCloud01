980b00357949a204c5f5602aa9366aa62cf761f1
jack zhang <zzy2240769410@gmail.com>
2019-06-19 22:06:29 +0800

    Initial commit

---
+++ src/main/java/com/zzy/controller/Test1.java
@@ -0,0 +1,16 @@
+package com.zzy.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+    static int i;
+
+    public static void main(String[] args) {
+        int k;
+        System.out.println(i);
+    }
+    @Test
+    public void stu(){
+        System.out.println(i);
+    }
+}
